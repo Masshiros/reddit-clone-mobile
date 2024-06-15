366a66d91a6164a1dc0c61a037f88d8355047119
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-06-16 00:36:36 +0700

    Feat(base): base folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:reddit_mobile/theme/palette.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,10 +13,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: Palette.darkModeAppTheme,
       home: const SizedBox(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
