263a2b7bb65a79c941bb2327fe947a1538d8957c
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-06-17 10:26:36 +0700

    Feat(auth): Config firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:reddit_mobile/features/auth/screens/login.screen.dart';
+import 'package:reddit_mobile/firebase_options.dart';
 import 'package:reddit_mobile/theme/palette.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
