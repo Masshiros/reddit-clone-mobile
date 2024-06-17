6e416edb2c0a5b3850a5043be69bf693312672e5
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-06-17 14:29:37 +0700

    Feat(auth): Add store user to firebase logic

--- lib/features/auth/repository/auth.repository.dart
+++ lib/features/auth/repository/auth.repository.dart
@@ -2,7 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:reddit_mobile/core/constants/constants.dart';
+import 'package:reddit_mobile/core/constants/firebase.constants.dart';
 import 'package:reddit_mobile/core/providers/firebase.provider.dart';
+import 'package:reddit_mobile/models/user.model.dart';
 
 final authRepositoryProvider = Provider(
   (ref) => AuthRepository(
@@ -23,6 +26,8 @@ class AuthRepository {
       : _fireStore = fireStore,
         _googleSignIn = googleSignIn,
         _auth = auth;
+  CollectionReference get _users =>
+      _fireStore.collection(FirebaseConstants.usersCollection);
   Future<void> signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
@@ -33,6 +38,19 @@ class AuthRepository {
       );
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
+      UserModel userModel;
+      if (userCredential.additionalUserInfo!.isNewUser) {
+        userModel = UserModel(
+          name: userCredential.user!.displayName ?? 'Anonymous',
+          profilePic: userCredential.user!.photoURL ?? Constants.avatarDefault,
+          banner: Constants.bannerDefault,
+          uid: userCredential.user!.uid,
+          isAuthenticated: true,
+          karma: 0,
+          awards: [],
+        );
+        await _users.doc(userModel.uid).set(userModel);
+      }
     } catch (e) {
       print(e);
     }
