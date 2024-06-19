25dd7e5b40664507f912aa0256d67564092881fa
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-06-19 09:35:05 +0700

    Feat(community): Add file_picker & pickImage utility

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,5 +1,13 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void showSnackBar(BuildContext context, String text) {
-  ScaffoldMessenger.of(context)..hideCurrentSnackBar()..showSnackBar(SnackBar(content: Text(text)));
+  ScaffoldMessenger.of(context)
+    ..hideCurrentSnackBar()
+    ..showSnackBar(SnackBar(content: Text(text)));
+}
+
+Future<FilePickerResult?> pickImage() async {
+  final image = await FilePicker.platform.pickFiles(type: FileType.image);
+  return image;
 }
