86e4cfc1391b67dcf08034e848079a949a2cb018
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-06-19 20:49:38 +0700

    Feat(profile): Edit profile functionality

---
+++ lib/features/profile/controller/profile.controller.dart
@@ -0,0 +1,80 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_mobile/core/providers/storage-firebase.provider.dart';
+import 'package:reddit_mobile/core/utils.dart';
+import 'package:reddit_mobile/features/auth/controller/auth.controller.dart';
+import 'package:reddit_mobile/features/profile/repository/profile.repository.dart';
+import 'package:reddit_mobile/models/user.model.dart';
+import 'package:routemaster/routemaster.dart';
+
+final userProfileControllerProvider =
+    StateNotifierProvider<UserProfileController, bool>((ref) {
+  final userProfileRepository = ref.watch(userProfileRepositoryProvider);
+  final storageRepository = ref.watch(storageRepositoryProvider);
+  return UserProfileController(
+    userProfileRepository: userProfileRepository,
+    storageRepository: storageRepository,
+    ref: ref,
+  );
+});
+
+class UserProfileController extends StateNotifier<bool> {
+  final UserProfileRepository _userProfileRepository;
+  final Ref _ref;
+  final StorageRepository _storageRepository;
+  UserProfileController({
+    required UserProfileRepository userProfileRepository,
+    required Ref ref,
+    required StorageRepository storageRepository,
+  })  : _userProfileRepository = userProfileRepository,
+        _ref = ref,
+        _storageRepository = storageRepository,
+        super(false);
+
+  void editCommunity({
+    required File? profileFile,
+    required File? bannerFile,
+    required BuildContext context,
+    required String name,
+  }) async {
+    state = true;
+    UserModel user = _ref.read(userProvider)!;
+
+    if (profileFile != null) {
+      final res = await _storageRepository.storeFile(
+        path: 'users/profile',
+        id: user.uid,
+        file: profileFile,
+      );
+      res.fold(
+        (l) => showSnackBar(context, l.message),
+        (r) => user = user.copyWith(profilePic: r),
+      );
+    }
+
+    if (bannerFile != null) {
+      final res = await _storageRepository.storeFile(
+        path: 'users/banner',
+        id: user.uid,
+        file: bannerFile,
+      );
+      res.fold(
+        (l) => showSnackBar(context, l.message),
+        (r) => user = user.copyWith(banner: r),
+      );
+    }
+
+    user = user.copyWith(name: name);
+    final res = await _userProfileRepository.editProfile(user);
+    state = false;
+    res.fold(
+      (l) => showSnackBar(context, l.message),
+      (r) {
+        _ref.read(userProvider.notifier).update((state) => user);
+        Routemaster.of(context).pop();
+      },
+    );
+  }
+}
---
+++ lib/features/profile/repository/profile.repository.dart
@@ -0,0 +1,32 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:reddit_mobile/core/constants/firebase.constants.dart';
+import 'package:reddit_mobile/core/failure.dart';
+import 'package:reddit_mobile/core/providers/firebase.provider.dart';
+import 'package:reddit_mobile/core/type.defs.dart';
+import 'package:reddit_mobile/models/user.model.dart';
+
+final userProfileRepositoryProvider = Provider((ref) {
+  return UserProfileRepository(firestore: ref.watch(firestoreProvider));
+});
+
+class UserProfileRepository {
+  final FirebaseFirestore _firestore;
+  UserProfileRepository({required FirebaseFirestore firestore})
+      : _firestore = firestore;
+
+  CollectionReference get _users =>
+      _firestore.collection(FirebaseConstants.usersCollection);
+  
+
+  VoidEither editProfile(UserModel user) async {
+    try {
+      return right(_users.doc(user.uid).update(user.toMap()));
+    } on FirebaseException catch (e) {
+      throw e.message!;
+    } catch (e) {
+      return left(Failure(e.toString()));
+    }
+  }
+}
